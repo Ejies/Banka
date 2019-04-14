09a18e3a9efd3e247cbcf84bbc3530bb327ed0f1
Emmanuel Eshofonie <ejieshofonie@yahoo.com>
2019-04-14 16:58:07 +0100

    update api

--- api/index.js
+++ api/index.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-//routes
-import userRoutes from './routes/user.route';
+// routes
+import allRoutes from './routes/routes';
 
 const app = express();
 const PORT = 9001;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  return res.send('The API is working');
-});
+app.get('/', (req, res) => res.send({ message: 'The API is working' }));
 
 // handle routes
-app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/', allRoutes);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT}`);
-    
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
+
+export default app;
